8e599843c4c1c73390adbb066693f82d9e7e9f14
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-12-09 05:34:47 -0800

    memory test updates

---
+++ t-mem.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const {Mem} = require('./test.linux-x64-gnu.node');
+
+
+class Mem2 {
+  constructor(n) {
+    this.buffer = Buffer.alloc(n * 8);
+  }
+}
+
+let Ctor;
+const how = process.argv[2];
+if (how === 'rust') {
+  Ctor = Mem;
+} else if (how === 'node') {
+  Ctor = Mem2;
+} else {
+  // eslint-disable-next-line no-console
+  console.log('usage: node t-mem.js rust|node');
+  process.exit(1);
+}
+
+// eslint-disable-next-line no-console
+console.log(`executing with ${how} allocator`);
+
+// execute both to set baseline initialization costs.
+new Mem(1000000);
+new Mem2(1000000);
+
+let lastMemTime = Date.now();
+let i = process.memoryUsage();
+const initialRss = i.rss;
+let count = 0;
+
+setInterval(allocate, 1);
+
+function allocate() {
+  for (let i = 0; i < 2; i++) {
+    const m = new Ctor(1000000);
+    count += 1;
+  }
+  if (Date.now() - lastMemTime > 5000) {
+    lastMemTime = Date.now();
+    const m = process.memoryUsage();
+    // eslint-disable-next-line no-console
+    console.log(`${count} Δrss ${m.rss - initialRss} rss ${m.rss - i.rss} hu ${m.heapUsed - i.heapUsed} x ${m.external - i.external}`);
+    i = m;
+  }
+}
